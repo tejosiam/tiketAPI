8d9bed7f9a51e29ba731473aab32768c3efe472d
Tejo Siam <fdtejo@gmail.com>
2019-01-03 16:36:40 +0700

    ADD NEW function : - /location/create - /event/ticket/create - /transaction/purchase - /transaction/get_info

--- src/routes.php
+++ src/routes.php
@@ -12,7 +12,6 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
-
 $app->post("/event/create", function (Request $request, Response $response){ 
 	$new_event = $request->getParsedBody(); 
 	$sql1 = "INSERT INTO event(nm_event, id_location, start_schedule, end_schedule) VALUE (:nm_event, :id_location, :start_schedule,:end_schedule)"; 
@@ -39,7 +38,7 @@ $app->post("/event/create", function (Request $request, Response $response){
 
 $app->post("/event/getinfo", function (Request $request, Response $response){
 	$new_event = $request->getParsedBody(); 
-	$sql = "SELECT e.id,e.nm_event,b.nm_location,d.nm_tiket,c.nm_customer,e.start_schedule,e.end_schedule
+	$sql = "SELECT a.id,e.nm_event,b.nm_location,d.nm_tiket,c.nm_customer,e.start_schedule,e.end_schedule
 	FROM events a
 	INNER JOIN event e ON e.id = a.id_event
 	INNER JOIN locations b ON b.id = e.id_location 
@@ -52,4 +51,63 @@ $app->post("/event/getinfo", function (Request $request, Response $response){
 	$stmt->execute();
     $result = $stmt->fetchAll();
     return $response->withJson(["status" => "Ok", "events" => $result], 200);
+});
+
+$app->post("/location/create", function (Request $request, Response $response){ 
+	$new_event = $request->getParsedBody(); 
+	$sql1 = "INSERT INTO locations(nm_location) VALUE (:nm_location)"; 
+	$stmt = $this->db->prepare($sql1); 
+	$stmt->bindValue( ":nm_location", $new_event["nm_location"], PDO::PARAM_STR );
+
+	 if($stmt->execute())
+	       return $response->withJson(["status" => "Ok"], 200);
+	    
+	    return $response->withJson(["status" => "Error"], 200);
+});
+
+$app->post("/event/ticket/create", function (Request $request, Response $response){ 
+	$new_event = $request->getParsedBody(); 
+	$sql1 = "INSERT INTO tikets(nm_tiket, price, quota, id_event) VALUE (:nm_tiket, :price, :quota,:id_event)"; 
+	$stmt = $this->db->prepare($sql1); 
+	$stmt->bindValue( ":nm_tiket", $new_event["nm_tiket"], PDO::PARAM_STR );
+	$stmt->bindValue( ":price", $new_event["price"], PDO::PARAM_INT );
+	$stmt->bindValue( ":quota", $new_event["quota"], PDO::PARAM_STR );
+	$stmt->bindValue( ":id_event", $new_event["id_event"], PDO::PARAM_STR );
+
+	 if($stmt->execute())
+	       return $response->withJson(["status" => "Ok"], 200);
+	    
+	    return $response->withJson(["status" => "Error"], 200);
+});
+
+$app->post("/transaction/purchase", function (Request $request, Response $response){ 
+	$new_event = $request->getParsedBody();
+	$sql1 = "INSERT INTO transactions(nm_transaction, id_customer, id_event, id_tiket) VALUE (:nm_transaction, :id_customer, :id_event,:id_tiket)"; 
+	$stmt = $this->db->prepare($sql1); 
+	$stmt->bindValue( ":nm_transaction", $new_event["nm_transaction"], PDO::PARAM_STR );
+	$stmt->bindValue( ":id_customer", $new_event["id_customer"], PDO::PARAM_INT );
+	$stmt->bindValue( ":id_event", $new_event["id_event"], PDO::PARAM_STR );
+	$stmt->bindValue( ":id_tiket", $new_event["id_tiket"], PDO::PARAM_STR );
+
+	 if($stmt->execute())
+	       return $response->withJson(["status" => "Ok"], 200);
+	    
+	    return $response->withJson(["status" => "Error"], 200);
+});
+
+$app->post("/transaction/get_info", function (Request $request, Response $response){
+	$new_event = $request->getParsedBody(); 
+	$sql = "SELECT a.id,a.nm_transaction,b.nm_customer,c.nm_event,d.nm_tiket,c.start_schedule,c.end_schedule
+	FROM transactions a
+	INNER JOIN customers b ON b.id = a.id_customer
+	INNER JOIN event c ON c.id = a.id_event
+	INNER JOIN tikets d ON d.id = a.id_tiket
+	WHERE a.nm_transaction = :nm_transaction and a.id_customer = :id_customer and a.id_event = :id_event and status = 'process'"; 
+	$stmt = $this->db->prepare($sql);
+	$stmt->bindValue( ":nm_transaction", $new_event["nm_transaction"], PDO::PARAM_STR );
+	$stmt->bindValue( ":id_customer", $new_event["id_customer"], PDO::PARAM_INT );
+	$stmt->bindValue( ":id_event", $new_event["id_event"], PDO::PARAM_INT );
+	$stmt->execute();
+    $result = $stmt->fetchAll();
+    return $response->withJson(["status" => "Ok", "transactions" => $result], 200);
 });
